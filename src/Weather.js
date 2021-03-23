dc83492013b7436295c50a8c3a196aec1c6168a0
Jyotsna Jose <jyotsnamaria@gmail.com>
2021-03-23 16:34:10 -0500

    added search and header sections

--- src/Weather.js
+++ src/Weather.js
@@ -4,10 +4,19 @@ export default function Weather() {
   return (
     <div className="Weather">
       <section className="searchCity">
-        <form>
-          <input type="search" placeholder="Search a city.." />
+        <form className="input-group">
+          <input
+            type="search"
+            class="form-control"
+            placeholder="Search a city.."
+          />
           <input type="submit" value="Search" />
         </form>
+      </section>
+      <section>
+        <h1>Minnesota</h1>
+        <h6>Feb 28 2021, Sunday, 15:11 CST</h6>
+        <h6>Clear Sky</h6>
       </section>
     </div>
   );
