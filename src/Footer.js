d05ef68d92f2017313fecc87632109b7e9752d8d
Jyotsna Jose <jyotsnamaria@gmail.com>
2021-03-28 23:52:52 -0500

    Updated footer with GitHub link

--- src/Footer.js
+++ src/Footer.js
@@ -3,7 +3,13 @@ import "./Footer.css";
 export default function Footer() {
   return (
     <small className="Footer">
-      <a href="/">Open-cource code</a> by Jyotnsa Jose
+      <a
+        href="https://github.com/JyotsnaJose/react-weatherforecast-app"
+        target="_blank"
+      >
+        Open-cource code
+      </a>{" "}
+      by Jyotnsa Jose
     </small>
   );
 }
