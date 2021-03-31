a09953bc1ef094d9c3346f514d09e5a7881d0884
Jyotsna Jose <jyotsnamaria@gmail.com>
2021-03-30 23:28:01 -0500

    Updated date and time display in js file

--- src/FormattedDate.js
+++ src/FormattedDate.js
@@ -2,8 +2,48 @@ import React from "react";
 
 export default function FormattedDate(props) {
   let date = new Date(props.date);
-  let day = date.getDay();
-  let hours = date.getHours();
-  let minutes = date.getMinutes();
-  return <div className="FormattedDate">{props.date}</div>;
+  let newDate = {
+    month: date.getMonth(),
+    date_today: date.getDate(),
+    year: date.getFullYear(),
+    day: date.getDay(),
+    hours: date.getHours(),
+    minutes: date.getMinutes(),
+  };
+  let months = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "saturday",
+  ];
+  if (newDate.hours < 10) {
+    newDate.hours = `0${newDate.hours}`;
+  }
+  if (newDate.minutes < 10) {
+    newDate.minutes = `0${newDate.minutes}`;
+  }
+
+  return (
+    <div className="FormattedDate">
+      {months[newDate.month]} {newDate.date_today} {newDate.year},{" "}
+      {days[newDate.day]} {newDate.hours}:{newDate.minutes}
+    </div>
+  );
 }
